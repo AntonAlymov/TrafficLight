1d697d52b7e2787c5a9fe2f9e42cda3df1d3b6a6
Anton <anton.alymov87@gmail.com>
2020-02-10 17:28:16 +0400

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Антон Алымов on 10.02.2020.
+//  Copyright © 2020 Anton Alymov Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
